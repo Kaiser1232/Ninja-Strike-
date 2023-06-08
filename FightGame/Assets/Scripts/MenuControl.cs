6325fcc54739168648d0e22a88d1047cd739bcb0
Kaiser1232 <110255144+Kaiser1232@users.noreply.github.com>
2023-06-08 11:25:15 -0700

    added play button and quit button

---
+++ FightGame/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void PlayButton()
+
+    {
+        SceneManager.LoadScene("Main");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
