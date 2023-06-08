a6f3b671d40a6fc9de5a091128058e56d8c99b63
Kaiser1232 <110255144+Kaiser1232@users.noreply.github.com>
2023-06-08 14:06:25 -0700

    quit button added

--- FightGame/Assets/Scripts/MenuControl.cs
+++ FightGame/Assets/Scripts/MenuControl.cs
@@ -5,19 +5,47 @@ using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
-    public void PlayButton()
+    public AudioSource playButtonFX;
+    public AudioSource quitButtonFX;
+
+    public void Play()
 
     {
         SceneManager.LoadScene("Main");
     }
+
+
+
+
+    public void PlayButton()
+
+    {
+        playButtonFX.Play();
+        Invoke(nameof(Play), 1);
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
-    public void QuitButton()
+
+
+
+    public void Quit()
     {
         Application.Quit();
+    }
+
+    
+    public void QuitButton()
+    {
+        quitButtonFX.Play();
+        Invoke(nameof(Quit), 1);
     }
     // Update is called once per frame
     void Update()
